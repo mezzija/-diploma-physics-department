b50056d673cee148298383fef963bcccee2d79a1
Timofey Dulkin <timofey.dulkin@ontravelsolutions.com>
2021-03-23 21:44:33 +0300

    2d

--- Linear Convection-1d.py
+++ Linear Convection-1d.py
@@ -1,25 +1,4 @@
-import numpy
-from matplotlib import pyplot
-import time, sys
-
-nx = 41
-dx = 2 / (nx-1)
-nt = 25
-dt = .025
-c = 1
-
-u = numpy.ones(nx)
-u[int(.5 / dx):int(1 / dx + 1)] = 2
-
-pyplot.plot(numpy.linspace(0, 2, nx), u)
-
-
-un = numpy.ones(nx)
-
-for n in range(nt):
-    un = u.copy()
-    for i in range(1, nx):
-        u[i] = un[i] - c * dt / dx * (un[i] - un[i-1])
-
-pyplot.plot(numpy.linspace(0, 2, nx), u)
-pyplot.show()
+array1 = [2, 2, 3, 4, 5, 6, 7, 8, 9]
+array2 = [1, 1, 1, 15, 1, 1, 1, 1, 1]
+array1[1:-1] = 2*array2[1:-1]
+print(len(array2))
---
+++ Navier_Stokes_2D.py
@@ -0,0 +1,9 @@
+import numpy
+from matplotlib import pyplot
+
+# количество узлов по времени
+t_nodes = 10
+# количество пространственных узлов
+x_nodes = 10
+y_nodes = 10
+
